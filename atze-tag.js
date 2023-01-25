36c2deb9c1e990544c5506ba3c2e29f204bfd4cb
Markus Baersch <mail@markus-baersch.de>
2023-01-25 02:15:56 +0100

    Update atze-tag.js

--- atze-tag.js
+++ atze-tag.js
@@ -1,5 +1,6 @@
+/** Kampagnen-Landingpages und Zielseitenaufrufe vermessen */
 !function(l,a,z,e,r){e="/atze/"
-a=/(gclid|wbraid)=.+/;z=/(danke|thank-you)\.html/
+a=/(&|\?)(gclid|wbraid)=.+/;z=/(danke|thank-you)\.html/
 l=document.location.href;r=(l.match(a))?"a":(l.match(z))?"z":null
 if(!r) return;(new Image()).src=e+"?"+r+"="+Math.random() 
 }();
